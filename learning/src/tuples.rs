dfd6ff2590492ed09d4e0152042bf43bf5934d4c
Bastian Karge <bastian.karge@gmail.com>
2022-05-16 16:02:50 +0200

    removed learning and tests

--- learning/src/tuples.rs
+++
@@ -1,9 +0,0 @@
-pub fn run()
-{
-    let person: (&str, &str, i8) = ("lol", "lol2", 10);
-    let person2 = ("test", "test", 10, 1034234);
-
-    println!("0: {}, 1: {}, 2: {}", person.0, person.1, person.2);
-    println!("{:?}", person);
-    println!("{:?}", person2);
-}
